2015db8450f18ea93bc98eade2f18da3ceab4891
Max Niederhoefer <max.niederhoefer@actiware.de>
2024-07-23 07:59:32 +0200

    [add] Template

---
+++ Template.cs
@@ -0,0 +1,12 @@
+﻿namespace adventofcode;
+
+public class Template : IDay {
+    public void GetFirstPart() {
+        throw new NotImplementedException();
+    }
+
+    public void GetSecondPart() {
+        throw new NotImplementedException();
+    }
+}
+
