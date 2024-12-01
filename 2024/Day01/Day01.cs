2942ec9f59850d926acaacf184e467116c4041a5
Max Niederhoefer <max.niederhoefer@actiware.de>
2024-12-01 12:22:47 +0100

    add 2024 day01 part2

--- 2024/Day01/Day01.cs
+++ 2024/Day01/Day01.cs
@@ -34,7 +34,44 @@
         }
 
         public void GetSecondPart() {
-            throw new NotImplementedException();
+            List<int> leftList = new();
+            List<int> rightList = new();
+
+            foreach (string line in File.ReadLines(path)) {
+                string[] parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length != 2) {
+                    continue;
+                }
+
+                if (int.TryParse(parts[0], out int left) && int.TryParse(parts[1], out int right)) {
+                    leftList.Add(left);
+                    rightList.Add(right);
+                }
+            }
+
+            int similarityScore = CalculateSimilarityScore(leftList, rightList);
+            Console.WriteLine($"Part2: " + similarityScore);
+        }
+
+        static int CalculateSimilarityScore(List<int> leftList, List<int> rightList) {
+            Dictionary<int, int> rightCount = new();
+
+            foreach (int num in rightList) {
+                if (rightCount.ContainsKey(num)) {
+                    rightCount[num]++;
+                } else {
+                    rightCount[num] = 1;
+                }
+            }
+
+            int similarityScore = 0;
+            foreach (int num in leftList) {
+                if (rightCount.ContainsKey(num)) {
+                    similarityScore += num * rightCount[num];
+                }
+            }
+
+            return similarityScore;
         }
     }
 }
