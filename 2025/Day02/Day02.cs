5c9a7cad95742fc89ef2f8d15639d834fbdb1687
Max Niederhoefer <max.niederhoefer@actiware.de>
2025-12-02 12:11:08 +0100

    refactor day2

--- 2025/Day02/Day02.cs
+++ 2025/Day02/Day02.cs
@@ -5,31 +5,16 @@ namespace adventofcode._2025;
 public class Day02 : IDay {
     string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "2025", "Day02", "input.txt");
     public void GetFirstPart() {
-        long sum = 0;
-        var regex = new Regex(@"^(\d+)\1$");
-
-        string lines = File.ReadAllText(path);
-
-        foreach (string range in lines.Split(',', StringSplitOptions.RemoveEmptyEntries)) {
-            var parts = range.Split('-', StringSplitOptions.RemoveEmptyEntries);
-            long start = long.Parse(parts[0]);
-            long end = long.Parse(parts[1]);
-
-            for (long id = start; id <= end; id++) {
-                if (regex.IsMatch(id.ToString())) {
-                    sum += id;
-                }
-            }
-        }
-
-        Console.WriteLine("Part1: " + sum);
+        Console.WriteLine("Part 1: " + GetInvalidIDs(new Regex(@"^(\d+)\1$")));
     }
 
     public void GetSecondPart() {
-        long sum = 0;
-        var regex = new Regex(@"^(\d+)\1+$");
+        Console.WriteLine("Part 2: " + GetInvalidIDs(new Regex(@"^(\d+)\1+$")));
+    }
 
+    private long GetInvalidIDs(Regex regex) {
         string lines = File.ReadAllText(path);
+        long sum = 0;
 
         foreach (string range in lines.Split(',', StringSplitOptions.RemoveEmptyEntries)) {
             var parts = range.Split('-', StringSplitOptions.RemoveEmptyEntries);
@@ -43,7 +28,7 @@ public class Day02 : IDay {
             }
         }
 
-        Console.WriteLine("Part1: " + sum);
+        return sum;
     }
 }
 
