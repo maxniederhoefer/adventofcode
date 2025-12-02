818219ba4669cb5ac1c0d3f66502c0ba68423a66
Max Niederhoefer <max.niederhoefer@actiware.de>
2025-12-02 12:06:20 +0100

    day 02 part 2

--- 2025/Day02/Day02.cs
+++ 2025/Day02/Day02.cs
@@ -26,7 +26,24 @@ public class Day02 : IDay {
     }
 
     public void GetSecondPart() {
-        throw new NotImplementedException();
+        long sum = 0;
+        var regex = new Regex(@"^(\d+)\1+$");
+
+        string lines = File.ReadAllText(path);
+
+        foreach (string range in lines.Split(',', StringSplitOptions.RemoveEmptyEntries)) {
+            var parts = range.Split('-', StringSplitOptions.RemoveEmptyEntries);
+            long start = long.Parse(parts[0]);
+            long end = long.Parse(parts[1]);
+
+            for (long id = start; id <= end; id++) {
+                if (regex.IsMatch(id.ToString())) {
+                    sum += id;
+                }
+            }
+        }
+
+        Console.WriteLine("Part1: " + sum);
     }
 }
 
